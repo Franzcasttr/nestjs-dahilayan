22ef4425e8cf90ef8c9738bf38da7b58810cfcde
Franzcasttr <franciscastro.216.fc@gmail.com>
2024-04-13 04:19:41 +0800

    remove firebase initializer

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import admin, { ServiceAccount } from 'firebase-admin';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost/3000',
     credentials: true,
-  });
-
-  const adminConfig: ServiceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  };
-
-  admin.initializeApp({
-    credential: admin.credential.cert(adminConfig),
   });
 
   await app.listen(8000);
